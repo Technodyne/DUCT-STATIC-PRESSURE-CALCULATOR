ee4ad473126968c655fe74e22d7de484bce5968a
Technodyne <murtuzanadiadwala@gmail.com>
2026-03-26 22:49:31 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,48 @@
+const CACHE_NAME = 'esp-calc-v1';
+
+// Files to cache for offline use
+const ASSETS_TO_CACHE = [
+  './',
+  './index.html', // Note: ensure your HTML file is named index.html, or update this line
+  './manifest.json',
+  './icon.png',
+  'https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js'
+];
+
+// Install event - caches the assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(ASSETS_TO_CACHE);
+      })
+  );
+});
+
+// Fetch event - serves files from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Return cached file if found, otherwise fetch from network
+        return response || fetch(event.request);
+      })
+  );
+});
+
+// Activate event - cleans up old caches when updating
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cache) => {
+          if (cache !== CACHE_NAME) {
+            console.log('Deleting old cache:', cache);
+            return caches.delete(cache);
+          }
+        })
+      );
+    })
+  );
+});
